087e66d1bcdce8c5ca93c98798bfa328fb175c4a
Litoxam <litobernier@gmail.com>
2026-04-08 10:55:33 +0200

    forgot to check if parent is NULL, updated

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -15,6 +15,9 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	binary_tree_t *old_child;
 	binary_tree_t *new_btn;
 
+	if (parent == NULL)
+		return (NULL);
+		
 	old_child = parent->right;
 
 
