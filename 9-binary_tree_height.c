eaca043c8b0e07ab0b9dd60d039865b04b97e7b1
Litoxam <litobernier@gmail.com>
2026-04-09 10:41:07 +0200

    Betty style : wrong indent and trailing whitespace

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,16 +12,16 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-		
+
 	if (tree->left)
 	{
-	    left_height = 1 + binary_tree_height(tree->left);
+	left_height = 1 + binary_tree_height(tree->left);
 	}
 	if (tree->right)
 	{
-	   right_height = 1 + binary_tree_height(tree->right);
+	right_height = 1 + binary_tree_height(tree->right);
 	}
-	
+
 	if (left_height > right_height)
 		return (left_height);
 	else
